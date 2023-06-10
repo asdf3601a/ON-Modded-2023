686b6f53d3b633c05e6dd2605b10c52355d3740d
cute_panda <asdf3601a@gmail.com>
2023-06-10 21:41:26 +0800

    add creative lasso recipes

---
+++ kubejs/server_scripts/recipes.js
@@ -0,0 +1,15 @@
+onEvent(
+    'recipes',
+    event => {
+        event.shapeless(
+            'moblassos:creative_lasso',
+            [
+                'moblassos:contract',
+                'moblassos:diamond_lasso',
+                'minecraft:enchanted_golden_apple',
+                'moblassos:hostile_lasso',
+                'moblassos:emerald_lasso',
+            ]
+        )
+    }
+)
